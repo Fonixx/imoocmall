540e35e64a6f47c4e139398e2287f59af74e6eaa
fonix <895891023@qq.com>
2020-03-14 19:00:40 +0800

    vue父子组件同步 用$emit实现

---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div >
+    <button v-on:click="increament">+</button>
+    <button v-on:click="decreament">-</button>
+    <p>{{number}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props:["number"],
+  data () {
+    return {
+      number:1,
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+    increament(){this.$emit("incre");},
+    decreament(){this.$emit("decre");}
+
+  }
+}
+</script>
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,96 +1,31 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h1>父组件number:{{ number }}</h1>
+
+    <Counter v-bind:number="number" v-on:incre="increament" v-on:decre="decreament"></Counter>
   </div>
 </template>
 
 <script>
+import Counter from './Counter'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
+      number:10,
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components:{
+    Counter
+  },
+  methods:{
+    increament(){this.number++},
+    decreament(){this.number--}
   }
+
 }
 </script>
 
