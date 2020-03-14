903292d5e1f1ccd44ab3664eb843e77c78d9c095
fonix <895891023@qq.com>
2020-03-14 19:20:33 +0800

    Vue动态路由 以及router得mode设置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import GoodsList from '../views/GoodsList'
+//import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
+  //mode:'hash', //浏览路径加上#
+  mode:'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/goods/:goodsId/user/:name',
+      name: 'GoodsList',
+      component: GoodsList
     }
   ]
 })
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -1,5 +1,9 @@
 <template>
-
+  <div>
+    这是商品列表页面<br>
+    <span>{{$route.params.goodsId}}</span><br>
+    <span>{{$route.params.name}}</span>
+  </div>
 </template>
 
 <script>
